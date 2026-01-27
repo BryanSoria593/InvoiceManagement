e1f6897d8fcf080534ffcb404e5caed6e6b8ca9b
Bryan Soria Méndez <zansoriam@gmail.com>
2026-01-26 21:57:04 -0500

    ✨ feat: add initial payment methods data to the database

---
+++ InvoiceManagementDatabase/initial.sql
@@ -0,0 +1,6 @@
+INSERT INTO PaymentMethods (Name, Status, CreatedAt, UpdatedAt, IsDeleted)
+VALUES 
+('Efectivo', 31, GETDATE(), GETDATE(), 0),
+('Cheque', 31, GETDATE(), GETDATE(), 0),
+('Transferencia bancaria', 31, GETDATE(), GETDATE(), 0),
+('Crédito', 31, GETDATE(), GETDATE(), 0);
