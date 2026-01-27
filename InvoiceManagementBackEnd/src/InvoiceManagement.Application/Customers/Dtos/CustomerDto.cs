7f81dcc2885a2d577fd1c154178cd11ba2b09766
Bryan Soria Méndez <zansoriam@gmail.com>
2026-01-27 02:18:08 -0500

    ✨ feat: make UpdatedAt property nullable in Customer entity and update migration files

--- InvoiceManagementBackEnd/src/InvoiceManagement.Application/Customers/Dtos/CustomerDto.cs
+++ InvoiceManagementBackEnd/src/InvoiceManagement.Application/Customers/Dtos/CustomerDto.cs
@@ -9,7 +9,7 @@ public class CustomerDto
     public string Email { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public CustomerStatus Status { get; set; }
     public bool IsDeleted { get; set; }
 }
