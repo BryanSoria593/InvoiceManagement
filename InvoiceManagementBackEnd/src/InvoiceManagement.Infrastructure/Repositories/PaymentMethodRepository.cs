dccc6d4c43a2836968f449e54a4a3e45bed73304
Bryan Soria Méndez <zansoriam@gmail.com>
2026-01-27 21:23:01 -0500

    ✨ feat: refactor PaymentMethodRepository to retrieve payment methods from the database instead of hardcoded values

--- InvoiceManagementBackEnd/src/InvoiceManagement.Infrastructure/Repositories/PaymentMethodRepository.cs
+++ InvoiceManagementBackEnd/src/InvoiceManagement.Infrastructure/Repositories/PaymentMethodRepository.cs
@@ -1,17 +1,19 @@
 using InvoiceManagement.Domain.PaymentMethods.Entities;
 using InvoiceManagement.Domain.PaymentMethods.Interfaces;
+using InvoiceManagement.Infrastructure.Persistence;
 
 namespace InvoiceManagement.Infrastructure.Repositories;
 public class PaymentMethodRepository : IPaymentMethodRepository
 {
+    private readonly InvoiceManagementDbContext _context;
+
+    public PaymentMethodRepository(InvoiceManagementDbContext context)
+    {
+        _context = context;
+    }
+
     public List<PaymentMethod> GetAll()
     {
-        return new List<PaymentMethod>
-        {
-            new PaymentMethod { Id = 1, Name = "Efectivo", Status = PaymentMethodStatus.Active, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow },
-            new PaymentMethod { Id = 2, Name = "Cheque", Status = PaymentMethodStatus.Active, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow },
-            new PaymentMethod { Id = 3, Name = "Transferencia bancaria", Status = PaymentMethodStatus.Active, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow },
-            new PaymentMethod { Id = 4, Name = "Crédito", Status = PaymentMethodStatus.Active, CreatedAt = DateTime.UtcNow, UpdatedAt = DateTime.UtcNow }
-        };
+        return _context.PaymentMethods.ToList();
     }
 }
